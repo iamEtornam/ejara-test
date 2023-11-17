086444655e7667183115df04c398d0ab18ece17d
iamEtornam <sunumacbright@gmail.com>
2023-11-17 15:15:48 +0000

    add required configs

--- lib/config/config_example.dart
+++ lib/config/config_example.dart
@@ -1,4 +1,9 @@
 class Config {
   static const String appName = "Ejara Assignment";
   static const String baseUrl = '<BASE_URL_HERE>';
+  static const String apiKey = '<API KEY HERE>';
+  static const String clientId = '<CLIENT ID HERE>';
+  static const String appVersion = '<APP VERSION HERE>';
+  static const String appPlatform = '<APP PLATFORM HERE>';
+  static const String client = '<CLIENT HERE>';
 }
