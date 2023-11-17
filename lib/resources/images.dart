a187047d7948207b761a081e865dae15f9f1c33e
iamEtornam <sunumacbright@gmail.com>
2023-11-17 17:47:15 +0000

    add image assets

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -2,4 +2,7 @@ part of 'resources.dart';
 
 class Images {
   Images._();
+
+  static const String icBank = 'assets/images/ic_bank.png';
+  static const String icFolder = 'assets/images/ic_folder.png';
 }
